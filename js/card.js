a606ebb00d0c1b4e896bba7bd725326b5b50e6da
ttomczak <ttomczak1512@protonmail.com>
2023-05-04 01:25:40 -0500

    update

--- js/card.js
+++ js/card.js
@@ -1,27 +1,21 @@
 const generateCard = function() {
-    let url ="https://gamingquizapi.azurewebsites.net/api/random/card";
- 
-     fetch(url)
-     .then(function(response) {
-         return response.json(); 
-     }) // Getting the raw JSON data
-     .then(function(data) {
- 
-         // Storing the card internally upon 
-         // successful completion of request
-         this.data = data; 
- 
-         // Displaying the card When the Webpage loads
-         console.log(this.data);
-         document.getElementById("question").innerHTML = this.data[0].question;
-         document.getElementById("answer").innerHTML = this.data[0].answer;
- }).catch(function() {
-    console.log("error");
- });
- 
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch("https://gamingquizapi.azurewebsites.net/api/random/card", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            this.data = data;
+            document.getElementById("question").innerHTML = this.data[0].question;
+            document.getElementById("answer").innerHTML = this.data[0].answer;
+        })
+        .catch(error => console.log('error', error));
 }
+
 window.onload = function() {
-     generateCard();
-     document.getElementById("generate").addEventListener('click', generateCard);
+    generateCard();
+    document.getElementById("generate").addEventListener('click', generateCard);
 }
- 
+
